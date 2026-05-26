701e1faf209618dc185be33f11561648097491cd
mjl060410-boop <mjl060410@gmail.com>
2026-05-26 17:45:14 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -7,3 +7,14 @@ st.title('첫 번째 웹사이트')
 "#첫 번째 웹사이트"
 
 
+'# :blue[시각화 라이브러리]'
+
+'##### :orange[Matplotlib: st.pyplot()]'
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.linspace(0,10,100)
+y=np.sin(x)
+
+fig, ax=plt.subplots()
+ax.plot(x,y)
+st.pyplot(fig)
